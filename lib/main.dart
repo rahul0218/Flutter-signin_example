da70079e3f1fd0a410ad1dda468b80d666792f5d
Rahul kumar <kumar.rahul0218@gmail.com>
2020-09-19 17:04:30 +0530

    Signup

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class SignUpApp extends StatelessWidget {
     return MaterialApp(
       routes: {
         '/': (context) => SignUpScreen(),
+        '/welcome': (context) => WelcomeScreen(),
       },
     );
   }
@@ -30,6 +31,16 @@ class SignUpScreen extends StatelessWidget {
   }
 }
 
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text('Welcome!', style: Theme.of(context).textTheme.headline2),
+    ));
+  }
+}
+
 class SignUpForm extends StatefulWidget {
   @override
   _SignUpFormState createState() => _SignUpFormState();
@@ -41,7 +52,11 @@ class _SignUpFormState extends State<SignUpForm> {
   final _usernameTextController = TextEditingController();
 
   double _formProgress = 0;
-
+  void _showWelcomeScreen() {
+    Navigator.of(context).pushNamed('/welcome');
+  }
+  
+  
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -74,7 +89,7 @@ class _SignUpFormState extends State<SignUpForm> {
           FlatButton(
             color: Colors.blue,
             textColor: Colors.white,
-            onPressed: null,
+            onPressed: _showWelcomeScreen,
             child: Text('Sign up'),
           ),
         ],
